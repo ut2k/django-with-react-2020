79b329782bbfa52372d172a709f46d5576903e89
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-06-28 16:44:37 -0500

    its working but setting up is a pain

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
+    url(r'^go', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    # url(r'^api-auth/', include('rest_framework.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
 ]
